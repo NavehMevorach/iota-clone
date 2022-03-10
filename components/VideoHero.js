e3ffe4df0f08a0b8479ab9d838055306693cfd60
Naveh Mevorach <navehmevorach@gmail.com>
2022-03-10 18:48:50 +0200

    changes

--- components/VideoHero.js
+++ components/VideoHero.js
@@ -6,7 +6,7 @@ function VideoHero({ videoSrc, isActive }) {
       className={`${
         isActive
           ? 'fadeIn w-screen h-screen -z-10 absolute top-0 left-0 m-0 p-0 -z-1 overflow-hidden'
-          : 'fadeOut'
+          : 'hidden fadeOut'
       }`}>
       <video
         src={videoSrc}
